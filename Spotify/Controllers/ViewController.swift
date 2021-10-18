25972c251c393cce4e815bac096361817ac267ce
Tarık Nasuhoğlu <tnasuh@gmail.com>
2021-10-18 03:17:50 +0300

    project initialized and storyboard removed

---
+++ Spotify/Controllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Tarık Nasuhoğlu on 18.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "home"
+        view.backgroundColor = .red
+    }
+
+
+}
+
