300f2fde7d833e46caaddcb661caf64c57e74b25
Tarık Nasuhoğlu <tnasuh@gmail.com>
2021-10-18 04:39:53 +0300

    Spotify API Keys plist added .gitignore file. get ClientSecret and getClientId methods created in AuthManager

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -10,6 +10,36 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        private static var clientID : String = "" // = "56c8955b963e43b69ee08ed36553cb4e"
+        private static var clientSecret: String = "" // = "da90a6ea506e44478feefb8381ddc498"
+        
+        static func getClientID() -> String {
+            if clientID.isEmpty {
+                var nsDictionary: NSDictionary?
+                 if let path = Bundle.main.path(forResource: "ClientConfig", ofType: "plist") {
+                    nsDictionary = NSDictionary(contentsOfFile: path)
+                     if let id = nsDictionary?.value(forKey: "ClientID") {
+                         clientID = id as! String
+                     }
+                 }
+            }
+            return clientID
+        }
+        static func getClientSecret() -> String {
+            if clientSecret.isEmpty {
+                var nsDictionary: NSDictionary?
+                 if let path = Bundle.main.path(forResource: "ClientConfig", ofType: "plist") {
+                    nsDictionary = NSDictionary(contentsOfFile: path)
+                     if let id = nsDictionary?.value(forKey: "ClientSecret") {
+                         clientSecret = id as! String
+                     }
+                 }
+            }
+            return clientSecret
+        }
+    }
+    
     private init() {}
     
     var isSignedIn: Bool {
