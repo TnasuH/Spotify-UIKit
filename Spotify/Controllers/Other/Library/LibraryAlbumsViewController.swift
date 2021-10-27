6c76d3e26b2bed71a3e2dec1e87411fcbe80f2a7
Tarık Nasuhoğlu <tnasuh@gmail.com>
2021-10-27 03:11:30 +0300

    LibraryAlbumsVC and LibraryPlaylist are created. Then these are added in scrollView with toggle in LibraryViewController

---
+++ Spotify/Controllers/Other/Library/LibraryAlbumsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryAlbumsViewController.swift
+//  Spotify
+//
+//  Created by Tarık Nasuhoğlu on 27.10.2021.
+//
+
+import UIKit
+
+class LibraryAlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+    
+}
---
+++ Spotify/Controllers/Other/Library/LibraryPlaylistsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LibraryPlaylistsViewController.swift
+//  Spotify
+//
+//  Created by Tarık Nasuhoğlu on 27.10.2021.
+//
+
+import UIKit
+
+class LibraryPlaylistsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+
+
+}
